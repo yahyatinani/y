b3b51cf687022cfb8f3b16731e55aabf903e1386
Yahya <yahyatinani@gmail.com>
2020-09-01 23:59:07 +0100

    Implement null and null string Arb generator

---
+++ y-common/src/test/kotlin/com/github/whyrising/y/TestUtils.kt
@@ -0,0 +1,34 @@
+package com.github.whyrising.y
+
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.collections.shouldContain
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.property.Arb
+import io.kotest.property.arbitrary.Codepoint
+import io.kotest.property.arbitrary.arb
+import io.kotest.property.arbitrary.ascii
+import io.kotest.property.arbitrary.merge
+import io.kotest.property.arbitrary.string
+
+fun <A> Arb.Companion.nil(): Arb<A?> = arb(listOf<A?>(null)) {
+    generateSequence { null }
+}
+
+fun Arb.Companion.`string?`(
+    minSize: Int = 0,
+    maxSize: Int = 100,
+    codepoints: Arb<Codepoint> = Arb.ascii()
+): Arb<String?> = Arb.nil<String>()
+    .merge(Arb.string(minSize, maxSize, codepoints))
+
+class TestUtilsTest : FreeSpec({
+    "Arb nil generator should return null" {
+        Arb.nil<Int>().edgecases() shouldHaveSize 1
+        Arb.nil<String>().edgecases() shouldHaveSize 1
+        Arb.nil<Boolean>().edgecases() shouldHaveSize 1
+    }
+
+    "Arb string? generator should generate at least 1 null" {
+        Arb.`string?`().edgecases() shouldContain null
+    }
+})
