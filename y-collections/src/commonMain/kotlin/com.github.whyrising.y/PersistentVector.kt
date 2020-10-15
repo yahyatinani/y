acd0f9545e7db91d409c5d6843c5a1c36509c40a
Yahya <yahyatinani@gmail.com>
2020-10-15 16:45:51 +0100

    Implement conj() for PersistentVector

--- y-collections/src/commonMain/kotlin/com.github.whyrising.y/PersistentVector.kt
+++ y-collections/src/commonMain/kotlin/com.github.whyrising.y/PersistentVector.kt
@@ -1,5 +1,9 @@
 package com.github.whyrising.y
 
+import com.github.whyrising.y.PersistentVector.Node.EmptyNode
+
+const val SHIFT = 5
+const val BF = 32
 
 sealed class PersistentVector<out E>(
     internal val count: Int,
@@ -8,12 +12,60 @@ sealed class PersistentVector<out E>(
     internal val tail: Array<Any?>
 ) {
 
-    internal class Node<out T>(val array: Array<Any?>)
+    private fun pushTail(level: Int, parent: Node<E>, tail: Node<E>): Node<E> {
+        val rootNode = Node<E>(parent.array.copyOf())
+        val index = ((count - 1) ushr level) and 0x01f
+        rootNode.array[index] = tail
+
+        return rootNode
+    }
+
+
+    fun conj(e: @UnsafeVariance E): PersistentVector<E> {
+        // empty slot available in tail?
+        if (count - tailOffset(count) < BF) {
+            val newTail = tail.copyOf(tail.size + 1)
+            newTail[tail.size] = e
+
+            return Vector(count + 1, SHIFT, root, newTail)
+        }
+
+        val tailNode = Node<E>(tail)
+        var newShift = shift
+        val newRoot: Node<E>
+
+        when {
+            // root overflow?
+            (count ushr SHIFT) > (1 shl shift) -> {
+                newRoot = Node()
+                newRoot.array[0] = root
+                newRoot.array[1] = newPath(shift, tailNode)
+                newShift += SHIFT
+            }
+            else -> newRoot = pushTail(shift, root, tailNode)
+        }
+
+        return Vector(count + 1, newShift, newRoot, arrayOf(e))
+    }
+
+    internal sealed class Node<out T>(val array: Array<Any?>) {
+        internal class Node2<out T>(_array: Array<Any?>) :
+            Node<T>(_array)
+
+        internal object EmptyNode :
+            PersistentVector.Node<Nothing>(arrayOfNulls(BF))
+
+        companion object {
+            operator fun <T> invoke(): Node2<T> = Node2(arrayOfNulls(BF))
+
+            operator fun <T> invoke(nodes: Array<Any?>): Node2<T> = Node2(nodes)
+        }
+    }
 
     internal abstract class AEmptyPersistentVector<E> : PersistentVector<E>(
         0,
-        5,
-        Node(arrayOfNulls(32)),
+        SHIFT,
+        EmptyNode,
         arrayOfNulls(0)
     ) {
         override fun toString(): String = "[]"
@@ -21,7 +73,38 @@ sealed class PersistentVector<out E>(
 
     internal object EmptyPersistentVector : AEmptyPersistentVector<Nothing>()
 
+    internal class Vector<out E>(
+        _count: Int,
+        _shift: Int,
+        _root: Node<E>,
+        _tail: Array<Any?>
+    ) : PersistentVector<E>(_count, _shift, _root, _tail)
+
     companion object {
         operator fun <E> invoke(): PersistentVector<E> = EmptyPersistentVector
+
+        @Suppress("UNCHECKED_CAST")
+        operator fun <E> invoke(vararg args: E): PersistentVector<E> {
+            val argsCount = args.size
+
+            if (argsCount == 0) return EmptyPersistentVector
+
+            val tail = args as Array<Any?>
+            return Vector(argsCount, SHIFT, EmptyNode, tail)
+        }
+
+        private fun tailOffset(count: Int): Int = when {
+            count < BF -> 0
+            else -> ((count - 1) ushr SHIFT) shl SHIFT
+        }
+
+        private tailrec fun <E> newPath(level: Int, node: Node<E>): Node<E> {
+            if (level == 0) return node
+
+            val path = Node<E>()
+            path.array[0] = node
+
+            return newPath(level - SHIFT, path)
+        }
     }
 }
--- y-collections/src/jvmTest/kotlin/com/github/whyrising/y/PersistentVectorTest.kt
+++ y-collections/src/jvmTest/kotlin/com/github/whyrising/y/PersistentVectorTest.kt
@@ -1,29 +1,143 @@
 package com.github.whyrising.y
 
 import com.github.whyrising.y.PersistentVector.EmptyPersistentVector
+import com.github.whyrising.y.PersistentVector.Node
+import com.github.whyrising.y.PersistentVector.Node.EmptyNode
 import io.kotest.assertions.throwables.shouldThrowExactly
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.collections.shouldContainAll
 import io.kotest.matchers.ints.shouldBeExactly
 import io.kotest.matchers.nulls.shouldBeNull
+import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeSameInstanceAs
+import io.kotest.property.Arb
+import io.kotest.property.arbitrary.filter
+import io.kotest.property.arbitrary.int
+import io.kotest.property.arbitrary.list
+import io.kotest.property.checkAll
 
-class NodeTest : FreeSpec({
-    @Suppress("UNCHECKED_CAST")
-    "Node does have an array" {
-        val array = arrayOfNulls<Int>(33)
-
-        val node = PersistentVector.Node<Int>(array as Array<Any?>)
-
-        node.array shouldBeSameInstanceAs array
-    }
-})
+const val SHIFT = 5
 
 class PersistentVectorTest : FreeSpec({
+    "Node" - {
+        @Suppress("UNCHECKED_CAST")
+        "Node does have an array of nodes" {
+            val array = arrayOfNulls<Int>(33)
+
+            val node = PersistentVector.Node<Int>(array as Array<Any?>)
+
+            node.array shouldBeSameInstanceAs array
+        }
+
+        "Empty node" {
+            val emptyNode = EmptyNode
+            val nodes = emptyNode.array
+
+            nodes.size shouldBeExactly 32
+            nodes[0].shouldBeNull()
+            nodes[31].shouldBeNull()
+        }
+    }
 
     "PersistentVector" - {
         "invoke() should return the EmptyPersistentVector" {
             PersistentVector<Int>() shouldBe EmptyPersistentVector
+        }
+
+        "invoke(args)" - {
+            "when args count <= 32, it should add to the tail" {
+                checkAll(Arb.list(Arb.int(), 0..32)) { list: List<Int> ->
+                    val vec = PersistentVector(*list.toTypedArray())
+                    val tail = vec.tail
+
+                    if (vec.count == 0)
+                        vec shouldBeSameInstanceAs EmptyPersistentVector
+                    vec.shift shouldBeExactly SHIFT
+                    vec.count shouldBeExactly list.size
+                    vec.root shouldBeSameInstanceAs EmptyNode
+                    tail.size shouldBeExactly list.size
+                    tail shouldContainAll list
+                }
+            }
+        }
+
+        "conj(e)" - {
+            val i = 7
+            "when the vec size is < 32, it should add e to the tail" {
+                val ints = Arb.int().filter { it != i }
+                checkAll(Arb.list(ints, 0..31)) { list ->
+                    val vec = PersistentVector(*list.toTypedArray()).conj(i)
+                    val tail = vec.tail
+
+                    vec.shift shouldBeExactly SHIFT
+                    vec.count shouldBeExactly list.size + 1
+                    vec.root shouldBeSameInstanceAs EmptyNode
+                    tail.size shouldBeExactly list.size + 1
+                    tail[list.size] as Int shouldBeExactly i
+                }
+            }
+
+            @Suppress("UNCHECKED_CAST")
+            "when the tail is full, it should push tail into the vec" - {
+                "when size is 32 and there is enough room in the root" {
+                    val e = 99
+                    val list = (1..32).toList()
+                    val pv = PersistentVector(*list.toTypedArray())
+
+                    val vec = pv.conj(e)
+                    val tail = vec.tail
+                    val root = vec.root
+                    val firstNodeElements = (root.array[0] as Node<Int>).array
+
+                    vec.shift shouldBeExactly SHIFT
+                    vec.count shouldBeExactly 33
+                    root.array.size shouldBeExactly 32
+                    firstNodeElements shouldBeSameInstanceAs pv.tail
+                    for ((index, n) in list.withIndex())
+                        firstNodeElements[index] as Int shouldBeExactly n
+                    tail.size shouldBeExactly 1
+                    tail[0] as Int shouldBeExactly e
+                }
+
+                "when size is 64 and there is enough room in the root" {
+                    val e = 99
+                    val list1 = (1..32).toList()
+                    val list2 = (33..64).toList()
+                    val chunk1 = PersistentVector(*list1.toTypedArray())
+                    val chunk2 = list2.fold(chunk1) { v, i -> v.conj(i) }
+
+                    val vec = chunk2.conj(e)
+                    val root = vec.root
+                    val tail = vec.tail
+                    val firstNodeElements = (root.array[0] as Node<Int>).array
+                    val secondNodeElements = (root.array[1] as Node<Int>).array
+
+                    firstNodeElements shouldBeSameInstanceAs chunk1.tail
+                    secondNodeElements shouldBeSameInstanceAs chunk2.tail
+                    vec.count shouldBeExactly 65
+                    tail.size shouldBeExactly 1
+                    tail[0] as Int shouldBeExactly e
+                }
+
+                "root overflow" {
+                    val e = 99
+                    val list = (1..1056).toList()
+
+                    val vec = list.fold(PersistentVector<Int>()) { v, i ->
+                        v.conj(i)
+                    }.conj(e)
+                    val leaf33 = (vec.root.array[1] as Node<Int>).array[0]
+                        as Node<Int>
+
+                    vec.count shouldBeExactly list.size + 1
+                    vec.shift shouldBeExactly 10
+                    vec.tail[0] as Int shouldBeExactly e
+                    vec.root.array[0].shouldNotBeNull()
+                    leaf33.array[0] shouldBe 1025
+                    leaf33.array[31] shouldBe 1056
+                }
+            }
         }
     }
 
@@ -37,7 +151,7 @@ class PersistentVectorTest : FreeSpec({
         }
 
         "shift should be 5" {
-            PersistentVector<Int>().shift shouldBeExactly 5
+            PersistentVector<Int>().shift shouldBeExactly SHIFT
         }
 
         "tail should be an empty array of size 0" {
