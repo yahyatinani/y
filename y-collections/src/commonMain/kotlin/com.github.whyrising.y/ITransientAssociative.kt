e5ef3c4eddd56019f43eb3517ea17a29a53228a6
Yahya <yahyatinani@gmail.com>
2020-10-31 18:15:49 +0100

    Add assoc(k,v) to ITransientAssociative

--- y-collections/src/commonMain/kotlin/com.github.whyrising.y/ITransientAssociative.kt
+++ y-collections/src/commonMain/kotlin/com.github.whyrising.y/ITransientAssociative.kt
@@ -2,4 +2,8 @@ package com.github.whyrising.y
 
 interface ITransientAssociative<out K, out V> :
     ITransientCollection<Any?>,
-    ILookup<K, V>
+    ILookup<K, V> {
+
+    fun assoc(key: @UnsafeVariance K, value: @UnsafeVariance V):
+        ITransientAssociative<K, V>
+}
--- y-collections/src/commonMain/kotlin/com.github.whyrising.y/ITransientMap.kt
+++ y-collections/src/commonMain/kotlin/com.github.whyrising.y/ITransientMap.kt
@@ -3,7 +3,7 @@ package com.github.whyrising.y
 interface ITransientMap<out K, out V> : ITransientAssociative<K, V>,
     ConstantCount {
 
-    fun assoc(key: @UnsafeVariance K, value: @UnsafeVariance V):
+    override fun assoc(key: @UnsafeVariance K, value: @UnsafeVariance V):
         ITransientMap<K, V>
 
     fun dissoc(key: @UnsafeVariance K): ITransientMap<K, V>
