41b901545ff6633d8506f272b553127c1560c333
Yahya <yahyatinani@gmail.com>
2020-11-08 22:20:54 +0100

    Implement assoc() for BitMapIndexedNode

--- y-collections/src/commonMain/kotlin/com.github.whyrising.y/LeanMap.kt
+++ y-collections/src/commonMain/kotlin/com.github.whyrising.y/LeanMap.kt
@@ -10,7 +10,8 @@ class LeanMap {
             isMutable: AtomicBoolean,
             shift: Int,
             keyHash: Int,
-            pair: Pair<@UnsafeVariance K, @UnsafeVariance V>,
+            key: @UnsafeVariance K,
+            value: @UnsafeVariance V,
             leafFlag: Box
         ): Node<K, V>
     }
@@ -22,26 +23,147 @@ class LeanMap {
         val array: Array<Any?>
     ) : Node<K, V> {
 
+        fun mergeIntoSubNode(isMutable: AtomicBoolean,
+                             shift: Int,
+                             currentHash: Int,
+                             currentKey: @UnsafeVariance K,
+                             currentValue: @UnsafeVariance V,
+                             newHash: Int,
+                             key: @UnsafeVariance K,
+                             value: @UnsafeVariance V
+        ): Node<K, V> {
+            if (shift > 32 && currentHash == newHash)
+                return HashCollisionNode(
+                    isMutable,
+                    currentHash,
+                    2,
+                    arrayOf(currentKey, currentValue, key, value))
+            else {
+                val currentMask = mask(currentHash, shift)
+                val newMask = mask(newHash, shift)
+
+                when (currentMask) {
+                    newMask -> {
+                        val subNode = mergeIntoSubNode(isMutable,
+                            shift + 5,
+                            currentHash,
+                            currentKey,
+                            currentValue,
+                            newHash,
+                            key,
+                            value)
+
+                        return BMIN(
+                            isMutable,
+                            0,
+                            bitpos(currentHash, shift),
+                            arrayOf(subNode))
+                    }
+                    else -> return BMIN(
+                        isMutable,
+                        bitpos(currentHash, shift) or bitpos(newHash, shift),
+                        0,
+                        if (currentMask < newMask)
+                            arrayOf(currentKey, currentValue, key, value)
+                        else arrayOf(key, value, currentKey, currentValue))
+                }
+            }
+        }
+
+        private fun putNewNode(
+            isMutable: AtomicBoolean,
+            bitpos: Int,
+            subNode: Node<K, V>
+        ): Node<K, V> {
+            val oldIdx = 2 * bitmapNodeIndex(datamap, bitpos)
+            val newIdx = array.size - 2 - bitmapNodeIndex(nodemap, bitpos)
+            val newArray = arrayOfNulls<Any?>(array.size - 1)
+
+            val endIndex = newIdx + 2
+            array.copyInto(newArray, 0, 0, oldIdx)
+            array.copyInto(newArray, oldIdx, oldIdx + 2, endIndex)
+            newArray[newIdx] = subNode
+            array.copyInto(newArray, newIdx + 1, endIndex, array.size)
+
+            return BMIN(
+                isMutable,
+                datamap xor bitpos,
+                nodemap or bitpos,
+                newArray
+            )
+        }
+
+        internal fun updateArrayByIndex(
+            index: Int, value: Any?, isMutable: AtomicBoolean
+        ): BitMapIndexedNode<K, V> =
+            // TODO: Review : Consider using the thread id instead of isMutable
+            if (assertMutable(this.isMutable, isMutable)) {
+                array[index] = value
+                this
+            } else {
+                val newArray = array.copyOf()
+                newArray[index] = value
+                BMIN(isMutable, datamap, nodemap, newArray)
+            }
+
+        @Suppress("UNCHECKED_CAST")
+        @ExperimentalStdlibApi
         override fun assoc(
             isMutable: AtomicBoolean,
             shift: Int,
             keyHash: Int,
-            pair: Pair<@UnsafeVariance K, @UnsafeVariance V>,
+            key: @UnsafeVariance K,
+            value: @UnsafeVariance V,
             leafFlag: Box
         ): Node<K, V> {
             val bitpos = bitpos(keyHash, shift)
 
-            val arrSize = array.size
-            val index = (2 * bitmapNodeIndex(datamap, bitpos))
+            if ((datamap and bitpos) != 0) {
+                val index = bitmapNodeIndex(datamap, bitpos)
+                val keyIndex = 2 * index
+                val currentKey = array[keyIndex] as K
 
-            val newArray: Array<Any?> = arrayOfNulls(arrSize + 2)
-            array.copyInto(newArray, 0, 0, index)
-            newArray[index] = pair.first
-            newArray[index + 1] = pair.second
-            array.copyInto(newArray, index + 2, index, arrSize - index)
-            leafFlag.value = leafFlag
+                if (equiv(currentKey, key))
+                    return updateArrayByIndex(keyIndex + 1, value, isMutable)
 
-            return BMIN(isMutable, datamap or bitpos, nodemap, newArray)
+                val currentValue = array[keyIndex + 1] as V
+
+                val subNode = mergeIntoSubNode(
+                    isMutable,
+                    shift + 5,
+                    hasheq(currentKey),
+                    currentKey,
+                    currentValue,
+                    hasheq(key),
+                    key,
+                    value)
+
+                leafFlag.value = leafFlag
+
+                return putNewNode(isMutable, bitpos, subNode)
+            } else if ((nodemap and bitpos) != 0) {
+                val nodeIdx = array.size - 1 - bitmapNodeIndex(nodemap, bitpos)
+                val subNode = array[nodeIdx] as BitMapIndexedNode<K, V>
+
+                val newNode = subNode.assoc(
+                    isMutable, shift + 5, keyHash, key, value, leafFlag)
+
+                if (subNode == newNode) return this
+
+                return updateArrayByIndex(nodeIdx, newNode, isMutable)
+            } else {
+                val arraySize = array.size
+                val index = (2 * bitmapNodeIndex(datamap, bitpos))
+
+                val newArray: Array<Any?> = arrayOfNulls(arraySize + 2)
+                array.copyInto(newArray, 0, 0, index)
+                newArray[index] = key
+                newArray[index + 1] = value
+                array.copyInto(newArray, index + 2, index, arraySize)
+                leafFlag.value = leafFlag
+
+                return BMIN(isMutable, (datamap or bitpos), nodemap, newArray)
+            }
         }
 
         object EmptyBitMapIndexedNode : BitMapIndexedNode<Nothing, Nothing>(
@@ -61,6 +183,28 @@ class LeanMap {
 
             fun bitmapNodeIndex(bitmap: Int, bitpos: Int): Int =
                 (bitmap and (bitpos - 1)).countOneBits()
+
+            fun assertMutable(x: AtomicBoolean, y: AtomicBoolean) =
+                x == y && x.value
+        }
+    }
+
+    class HashCollisionNode<out K, out V>(
+        val isMutable: AtomicBoolean,
+        val hash: Int,
+        val count: Int,
+        val array: Array<Any?>
+    ) : Node<K, V> {
+
+        override fun assoc(
+            isMutable: AtomicBoolean,
+            shift: Int,
+            keyHash: Int,
+            key: @UnsafeVariance K,
+            value: @UnsafeVariance V,
+            leafFlag: Box
+        ): Node<K, V> {
+            TODO("Not yet implemented")
         }
     }
 
--- y-collections/src/jvmTest/kotlin/com/github/whyrising/y/hashmap/LeanMapTest.kt
+++ y-collections/src/jvmTest/kotlin/com/github/whyrising/y/hashmap/LeanMapTest.kt
@@ -3,16 +3,20 @@ package com.github.whyrising.y.hashmap
 import com.github.whyrising.y.Box
 import com.github.whyrising.y.LeanMap
 import com.github.whyrising.y.LeanMap.BitMapIndexedNode
+import com.github.whyrising.y.LeanMap.BitMapIndexedNode.BMIN
 import com.github.whyrising.y.LeanMap.BitMapIndexedNode.Companion.bitmapNodeIndex
 import com.github.whyrising.y.LeanMap.BitMapIndexedNode.EmptyBitMapIndexedNode
 import com.github.whyrising.y.LeanMap.Companion.bitpos
+import com.github.whyrising.y.LeanMap.HashCollisionNode
 import com.github.whyrising.y.hasheq
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.booleans.shouldBeFalse
 import io.kotest.matchers.ints.shouldBeExactly
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeSameInstanceAs
+import io.kotest.matchers.types.shouldNotBeSameInstanceAs
 import kotlinx.atomicfu.atomic
+
 
 @ExperimentalStdlibApi
 class LeanMapTest : FreeSpec({
@@ -42,6 +46,249 @@ class LeanMapTest : FreeSpec({
             bitmapNodeIndex(1077936128, 1) shouldBeExactly 0
         }
 
+        "mergeTwoKeyValuePairs()/pairToSubNode()/subNode()" - {
+            """when currentKeyMask < newKeyMask, return a node containing both
+               where current entry comes before the new entry
+            """ {
+                val shift = 5
+                val isMutable = atomic(true)
+                val leafFlag = Box(null)
+                val currentKey = "4"
+                val currentValue = 4
+                val currentHash = hasheq(currentKey)
+                val currentNode =
+                    BMIN<String, Int>(isMutable, 0, 5, emptyArray()).assoc(
+                        isMutable,
+                        shift,
+                        currentHash,
+                        currentKey,
+                        currentValue,
+                        leafFlag) as BitMapIndexedNode<String, Int>
+
+                val newKey = "8"
+                val newHash = hasheq(newKey)
+                val newValue = 8
+                val newDatamap = bitpos(currentHash, shift) or
+                    bitpos(newHash, shift)
+
+                val subNode = currentNode.mergeIntoSubNode(
+                    isMutable,
+                    shift,
+                    currentHash,
+                    currentKey,
+                    currentValue,
+                    newHash,
+                    newKey,
+                    newValue) as BitMapIndexedNode<String, Int>
+                val array = subNode.array
+
+                subNode.isMutable shouldBeSameInstanceAs isMutable
+                subNode.nodemap shouldBeExactly 0
+                subNode.datamap shouldBeExactly newDatamap
+                array.size shouldBeExactly 4
+                array[0] shouldBeSameInstanceAs currentKey
+                array[1] shouldBe currentValue
+                array[2] shouldBeSameInstanceAs newKey
+                array[3] shouldBe newValue
+            }
+
+            """when newKeyMask < currentKeyMask, return a node containing both
+               where new entry comes before the current entry
+            """ {
+                val shift = 5
+                val isMutable = atomic(true)
+                val leafFlag = Box(null)
+                val currentKey = "12"
+                val currentValue = 12
+                val currentHash = hasheq(currentKey)
+                val currentNode =
+                    BMIN<String, Int>(isMutable, 0, 5, emptyArray()).assoc(
+                        isMutable,
+                        shift,
+                        currentHash,
+                        currentKey,
+                        currentValue,
+                        leafFlag) as BitMapIndexedNode<String, Int>
+
+                val newKey = "18"
+                val newHash = hasheq(newKey)
+                val newValue = 18
+                val newDatamap = bitpos(currentHash, shift) or
+                    bitpos(newHash, shift)
+
+                val subNode = currentNode.mergeIntoSubNode(
+                    isMutable,
+                    shift,
+                    currentHash,
+                    currentKey,
+                    currentValue,
+                    newHash,
+                    newKey,
+                    newValue) as BitMapIndexedNode<String, Int>
+                val array = subNode.array
+
+                subNode.isMutable shouldBeSameInstanceAs isMutable
+                subNode.nodemap shouldBeExactly 0
+                subNode.datamap shouldBeExactly newDatamap
+                array.size shouldBeExactly 4
+                array[0] shouldBeSameInstanceAs newKey
+                array[1] shouldBe newValue
+                array[2] shouldBeSameInstanceAs currentKey
+                array[3] shouldBe currentValue
+            }
+
+            @Suppress("UNCHECKED_CAST")
+            "when newKeyMask = currentKeyMask, it should subNode again" {
+                val shift = 10
+                val isMutable = atomic(true)
+                val leafFlag = Box(null)
+                val currentKey = "410"
+                val currentValue = 410
+                val currentHash = hasheq(currentKey)
+                val currentNode =
+                    BMIN<String, Int>(isMutable, 0, 0, emptyArray()).assoc(
+                        isMutable,
+                        shift,
+                        currentHash,
+                        currentKey,
+                        currentValue,
+                        leafFlag) as BitMapIndexedNode<String, Int>
+
+                val newShift = 15
+                val newKey = "1140"
+                val newHash = hasheq(newKey)
+                val newValue = 1140
+                val newDatamap = bitpos(currentHash, newShift) or
+                    bitpos(newHash, newShift)
+                val newNodemap = bitpos(currentHash, shift)
+
+                val node = currentNode.mergeIntoSubNode(
+                    isMutable,
+                    shift,
+                    currentHash,
+                    currentKey,
+                    currentValue,
+                    newHash,
+                    newKey,
+                    newValue) as BitMapIndexedNode<String, Int>
+                val array = node.array
+                val subNode = array[0] as BitMapIndexedNode<String, Int>
+                val subNodeArray = subNode.array
+
+                node.isMutable shouldBeSameInstanceAs isMutable
+                node.nodemap shouldBeExactly newNodemap
+                node.datamap shouldBeExactly 0
+                array.size shouldBeExactly 1
+
+                subNode.isMutable shouldBeSameInstanceAs isMutable
+                subNode.nodemap shouldBeExactly 0
+                subNode.datamap shouldBeExactly newDatamap
+                subNodeArray.size shouldBeExactly 4
+                subNodeArray[0] shouldBeSameInstanceAs currentKey
+                subNodeArray[1] shouldBe currentValue
+                subNodeArray[2] shouldBeSameInstanceAs newKey
+                subNodeArray[3] shouldBe newValue
+            }
+
+            """when hash collision and shift > 32, it should return 
+               a HashCollisionNode""" {
+                val shift = 35
+                val isMutable = atomic(true)
+                val currentKey = "RwtM1oQGxE"
+                val currentValue = 158135874
+                val currentHash = hasheq(currentKey)
+                val leafFlag = Box(null)
+
+                val currentNode =
+                    BMIN<String, Int>(isMutable, 0, 0, emptyArray()).assoc(
+                        isMutable,
+                        shift,
+                        currentHash,
+                        currentKey,
+                        currentValue,
+                        leafFlag) as BitMapIndexedNode<String, Int>
+
+                val newKey = "J2RCvlt3yJ"
+                val newValue = 848159417
+                val newHash = hasheq(newKey)
+
+                val collisionNode =
+                    currentNode.mergeIntoSubNode(
+                        isMutable,
+                        shift,
+                        currentHash,
+                        currentKey,
+                        currentValue,
+                        newHash,
+                        newKey,
+                        newValue) as HashCollisionNode<String, Int>
+                val array = collisionNode.array
+
+                collisionNode.isMutable shouldBeSameInstanceAs isMutable
+                collisionNode.count shouldBeExactly 2
+                collisionNode.hash shouldBeExactly currentHash
+                array.size shouldBeExactly 4
+                array[0] shouldBeSameInstanceAs currentKey
+                array[1] shouldBe currentValue
+                array[2] shouldBeSameInstanceAs newKey
+                array[3] shouldBe newValue
+            }
+        }
+
+        "updateArrayByIndex(index, value)" - {
+            "when isMutable flags are not the same, return a new node updated" {
+                val shift = 0
+                val leafFlag = Box(null)
+                val node = BitMapIndexedNode<Number, String>()
+                    .assoc(atomic(true), shift, hasheq(4), 4, "4", leafFlag)
+                    as BitMapIndexedNode<Number, String>
+
+                val newNode = node.updateArrayByIndex(1, "z", atomic(true))
+
+                newNode shouldNotBeSameInstanceAs node
+                newNode.datamap shouldBeExactly node.datamap
+                newNode.nodemap shouldBeExactly node.nodemap
+                newNode.array.size shouldBeExactly 2
+                newNode.array[0] shouldBe 4
+                newNode.array[1] shouldBe "z"
+            }
+
+            """when isMutable flags are the same but set to false, 
+               return a new node updated""" {
+                val shift = 0
+                val leafFlag = Box(null)
+                val isMutable = atomic(false)
+                val node = BitMapIndexedNode<Number, String>()
+                    .assoc(isMutable, shift, hasheq(4), 4, "4", leafFlag)
+                    as BitMapIndexedNode<Number, String>
+
+                val newNode = node.updateArrayByIndex(1, "z", isMutable)
+
+                newNode shouldNotBeSameInstanceAs node
+                newNode.datamap shouldBeExactly node.datamap
+                newNode.nodemap shouldBeExactly node.nodemap
+                newNode.array.size shouldBeExactly 2
+                newNode.array[0] shouldBe 4
+                newNode.array[1] shouldBe "z"
+            }
+
+            "when it's allowed to mutate, update the value in array" {
+                val shift = 0
+                val leafFlag = Box(null)
+                val isMutable = atomic(true)
+                val node = BitMapIndexedNode<Number, String>()
+                    .assoc(isMutable, shift, hasheq(4), 4, "4", leafFlag)
+                    as BitMapIndexedNode<Number, String>
+
+                val newNode = node.updateArrayByIndex(1, "z", isMutable)
+
+                newNode shouldBeSameInstanceAs node
+                newNode.array.size shouldBeExactly 2
+                newNode.array[0] shouldBe 4
+                newNode.array[1] shouldBe "z"
+            }
+        }
+
         "assoc(isMutable, shift, keyHash, pair, leafFlag)" - {
             """when collision-free, it should add the pair to the first half 
                of the array""" {
@@ -50,12 +297,11 @@ class LeanMapTest : FreeSpec({
                 val shift = 0
                 val keyHash = hasheq(key)
                 val value = 18
-                val keyVal = Pair(key, value)
                 val leafFlag = Box(null)
                 val node = BitMapIndexedNode<String, Int>()
 
                 val newNode = node.assoc(
-                    isMutable, shift, keyHash, keyVal, leafFlag
+                    isMutable, shift, keyHash, key, value, leafFlag
                 ) as BitMapIndexedNode<String, Int>
                 val newArray = newNode.array
 
@@ -69,8 +315,162 @@ class LeanMapTest : FreeSpec({
                 newArray[1] shouldBe value
             }
 
-            "" {
+            @Suppress("UNCHECKED_CAST")
+            "when there is a collision in the first half of the array" - {
+                "when keys are not equiv, it should call mergeIntoSubNode()" {
+                    val key = "8"
+                    val value = 8
+                    val isMutable = atomic(true)
+                    val shift = 0
+                    val keyHash = hasheq(key)
+                    val leafFlag = Box(null)
+                    val bitpos = bitpos(keyHash, shift)
+                    val node = BitMapIndexedNode<String, Int>()
+                        .assoc(isMutable, shift, hasheq("0"), "0", 0, leafFlag)
+                        .assoc(isMutable, shift, hasheq("2"), "2", 2, leafFlag)
+                        .assoc(isMutable, shift, hasheq("4"), "4", 4, leafFlag)
+                        as BitMapIndexedNode<String, Int>
 
+                    val newNode = node
+                        .assoc(isMutable, shift, keyHash, key, value, leafFlag)
+                        as BitMapIndexedNode<String, Int>
+
+                    val newArray = newNode.array
+                    val subNode = newArray[4] as BitMapIndexedNode<String, Int>
+
+                    leafFlag.value shouldBeSameInstanceAs leafFlag
+                    newArray.size shouldBeExactly 5
+
+                    newNode.datamap shouldBeExactly (node.datamap xor bitpos)
+                    newNode.nodemap shouldBeExactly (node.nodemap or bitpos)
+
+                    newArray[0] shouldBe "0"
+                    newArray[1] shouldBe 0
+                    newArray[2] shouldBe "2"
+                    newArray[3] shouldBe 2
+
+                    subNode.array.size shouldBeExactly 4
+                    subNode.array[0] shouldBe "4"
+                    subNode.array[1] shouldBe 4
+                    subNode.array[2] shouldBe "8"
+                    subNode.array[3] shouldBe 8
+                }
+
+                "when keys are equiv" - {
+                    "when it's allowed to mutate, update the value in array" {
+                        val shift = 0
+                        val isMutable = atomic(true)
+                        val leafFlag = Box(null)
+                        val node = BitMapIndexedNode<Number, String>()
+                            .assoc(
+                                isMutable, shift, hasheq(4), 4, "4", leafFlag)
+                            as BitMapIndexedNode<Number, String>
+
+                        val newNode = node
+                            .assoc(
+                                isMutable, shift, hasheq(4L), 4L, "1", leafFlag)
+                            as BitMapIndexedNode<String, Int>
+
+                        newNode shouldBeSameInstanceAs node
+                        newNode.array.size shouldBeExactly 2
+                        newNode.array[0] shouldBe 4
+                        newNode.array[1] shouldBe "1"
+                    }
+
+                    "when it's not allowed to mutate, return new node updated" {
+                        val shift = 0
+                        val leafFlag = Box(null)
+                        val node = BitMapIndexedNode<Number, String>()
+                            .assoc(
+                                atomic(true),
+                                shift,
+                                hasheq(4),
+                                4,
+                                "4",
+                                leafFlag) as BitMapIndexedNode<Number, String>
+
+                        val newNode = node
+                            .assoc(
+                                atomic(true),
+                                shift,
+                                hasheq(4L),
+                                4L,
+                                "1",
+                                leafFlag) as BitMapIndexedNode<String, Int>
+
+                        node.array.size shouldBeExactly 2
+                        node.array[0] shouldBe 4
+                        node.array[1] shouldBe "4"
+                        newNode shouldNotBeSameInstanceAs node
+                        newNode.datamap shouldBeExactly node.datamap
+                        newNode.nodemap shouldBeExactly node.nodemap
+                        newNode.array.size shouldBeExactly 2
+                        newNode.array[0] shouldBe 4
+                        newNode.array[1] shouldBe "1"
+                    }
+                }
+            }
+
+            "when there is collision in the second half of the array" - {
+                @Suppress("UNCHECKED_CAST")
+                "it should add to the subNode and return a new node" {
+                    val shift = 0
+                    val isMutable = atomic(true)
+                    val leafFlag = Box(null)
+
+                    var i = 0
+                    var node = BitMapIndexedNode<String, Int>()
+                    while (i < 36) {
+                        val key = "$i"
+                        node = node.assoc(
+                            isMutable, shift, hasheq(key), key, i, leafFlag
+                        ) as BitMapIndexedNode<String, Int>
+                        i += 1
+                    }
+
+                    val newNode = node.assoc(
+                        isMutable, shift, hasheq("36"), "36", 36, leafFlag)
+                        as BitMapIndexedNode<String, Int>
+                    val newArray = newNode.array
+                    val subNode = newArray[24] as BitMapIndexedNode<String, Int>
+
+                    newNode shouldBeSameInstanceAs node
+                    newArray.size shouldBeExactly node.array.size
+                    subNode.array[2] shouldBe "36"
+                    subNode.array[3] shouldBe 36
+                }
+
+                @Suppress("UNCHECKED_CAST")
+                "it should put the subNode and return this node" {
+                    val shift = 0
+                    val isMutable = atomic(true)
+                    val leafFlag = Box(null)
+
+                    var i = 0
+                    var node = BitMapIndexedNode<String, Int>()
+                    while (i < 424) {
+                        val key = "$i"
+                        node = node.assoc(
+                            isMutable, shift, hasheq(key), key, i, leafFlag
+                        ) as BitMapIndexedNode<String, Int>
+                        i += 2
+                    }
+                    val array =
+                        ((node.array[5] as BitMapIndexedNode<String, Int>)
+                            .array[12] as BitMapIndexedNode<String, Int>).array
+
+                    val newNode = node.assoc(
+                        isMutable, shift, hasheq("424"), "424", 424, leafFlag)
+                        as BitMapIndexedNode<String, Int>
+                    val newArray =
+                        ((newNode.array[5] as BitMapIndexedNode<String, Int>)
+                            .array[12] as BitMapIndexedNode<String, Int>).array
+
+                    newNode shouldBeSameInstanceAs node
+                    newArray.size shouldBeExactly array.size + 2
+                    newArray[4] shouldBe "424"
+                    newArray[5] shouldBe 424
+                }
             }
         }
     }
