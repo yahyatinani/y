68a6148e915b6f64f5a78fd115659505dc6d3106
Yahya <yahyatinani@gmail.com>
2020-11-19 15:57:54 +0100

    Make IPersistentStack a subtype of IPersistentCollection

--- y-collections/src/commonMain/kotlin/com.github.whyrising.y/stack/IPersistentStack.kt
+++ y-collections/src/commonMain/kotlin/com.github.whyrising.y/stack/IPersistentStack.kt
@@ -1,6 +1,8 @@
 package com.github.whyrising.y.stack
 
-interface IPersistentStack<out E> {
+import com.github.whyrising.y.seq.IPersistentCollection
+
+interface IPersistentStack<out E> : IPersistentCollection<E> {
     fun peek(): E?
 
     fun pop(): IPersistentStack<E>
