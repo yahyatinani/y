c7c88be4768b1e8ba42f7de4311114efc547d0e8
Yahya <yahyatinani@gmail.com>
2021-08-10 13:20:51 +0100

    Update main build CI workflow

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,3 +10,16 @@ rootProject.name = "y"
 
 include(":y-core")
 include(":y-collections")
+
+plugins {
+    id("com.gradle.enterprise") version "3.6.1"
+    // See https://jmfayard.github.io/refreshVersions
+    // id("de.fayard.refreshVersions") version "0.11.0"
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
