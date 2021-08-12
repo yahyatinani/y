cdd992d32e4f584da7c3c5252bfd485844d0b96d
Yahya <yahyatinani@gmail.com>
2021-08-12 13:42:51 +0100

    Remove redundant code from core and collections build scripts

--- y-collections/build.gradle.kts
+++ y-collections/build.gradle.kts
@@ -50,70 +50,13 @@ kotlin {
             }
         }
 
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test"))
-                implementation(Libs.Kotest.assertions)
-                implementation(Libs.Kotest.propertyTest)
-            }
-        }
-
         val jvmMain by getting
         val jvmTest by getting {
             dependencies {
                 implementation(Libs.Kotest.runner)
                 implementation(Libs.Kotlinx.coroutines)
-            }
-        }
-
-        if (!ideaActive) {
-            val nativeMain by creating {
-                dependsOn(commonMain)
-            }
-            val nativeTest by creating
-
-            val macosX64Main by getting {
-                dependsOn(nativeMain)
-            }
-
-            val mingwX64Main by getting {
-                dependsOn(nativeMain)
-            }
-
-            val linuxX64Main by getting {
-                dependsOn(nativeMain)
-            }
-
-            val iosX64Main by getting {
-                dependsOn(nativeMain)
-            }
-
-            val iosArm64Main by getting {
-                dependsOn(nativeMain)
-            }
-
-            val iosArm32Main by getting {
-                dependsOn(nativeMain)
-            }
-
-            val watchosX86Main by getting {
-                dependsOn(nativeMain)
-            }
-
-            val watchosArm32Main by getting {
-                dependsOn(nativeMain)
-            }
-
-            val watchosArm64Main by getting {
-                dependsOn(nativeMain)
-            }
-
-            val watchosX64Main by getting {
-                dependsOn(nativeMain)
-            }
-
-            val tvosMain by getting {
-                dependsOn(nativeMain)
+                implementation(Libs.Kotest.assertions)
+                implementation(Libs.Kotest.propertyTest)
             }
         }
 
