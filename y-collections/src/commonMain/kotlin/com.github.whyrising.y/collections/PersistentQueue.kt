a65ceecc78dd6e9f03c32bd17b67d44cced42b2b
Yahya <47183730+whyrising@users.noreply.github.com>
2021-11-13 16:17:34 +0100

    Implement queue.conj() and reimplement the empty queue

--- y-collections/src/commonMain/kotlin/com.github.whyrising.y/collections/PersistentQueue.kt
+++ y-collections/src/commonMain/kotlin/com.github.whyrising.y/collections/PersistentQueue.kt
@@ -3,15 +3,44 @@ package com.github.whyrising.y.collections
 import com.github.whyrising.y.collections.concretions.list.PersistentList.Empty
 import com.github.whyrising.y.collections.concretions.vector.PersistentVector
 import com.github.whyrising.y.collections.concretions.vector.PersistentVector.EmptyVector
+import com.github.whyrising.y.collections.core.first
+import com.github.whyrising.y.collections.core.l
+import com.github.whyrising.y.collections.seq.IPersistentCollection
 import com.github.whyrising.y.collections.seq.ISeq
+import com.github.whyrising.y.collections.stack.IPersistentStack
 
 class PersistentQueue<out E> private constructor(
-    val count: Int,
+    override val count: Int,
     val front: ISeq<E>,
     val back: PersistentVector<E>
-) {
+) : IPersistentStack<E> {
+
+    override fun peek(): E? = TODO("Not yet implemented")
+
+    override fun pop(): IPersistentStack<E> {
+        TODO("Not yet implemented")
+    }
+
+    override fun conj(e: @UnsafeVariance E): PersistentQueue<E> = when (front) {
+        is Empty -> PersistentQueue(count + 1, l(e), EmptyVector)
+        else -> PersistentQueue(count + 1, front, back.conj(e))
+    }
+
+    override fun empty(): IPersistentCollection<E> {
+        TODO("Not yet implemented")
+    }
+
+    override fun equiv(other: Any?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun seq(): ISeq<E> {
+        TODO("Not yet implemented")
+    }
+
     companion object {
-        operator fun <E> invoke(): PersistentQueue<E> =
-            PersistentQueue<E>(0, Empty, EmptyVector)
+        private val EMPTY_QUEUE = PersistentQueue(0, Empty, EmptyVector)
+
+        operator fun <E> invoke(): PersistentQueue<E> = EMPTY_QUEUE
     }
 }
--- y-collections/src/commonTest/kotlin/com/github/whyrising/y/collections/PersistentQueueTest.kt
+++ y-collections/src/commonTest/kotlin/com/github/whyrising/y/collections/PersistentQueueTest.kt
@@ -2,7 +2,10 @@ package com.github.whyrising.y.collections
 
 import com.github.whyrising.y.collections.concretions.list.PersistentList
 import com.github.whyrising.y.collections.concretions.vector.PersistentVector
+import com.github.whyrising.y.collections.core.l
+import com.github.whyrising.y.collections.core.v
 import io.kotest.matchers.ints.shouldBeExactly
+import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeSameInstanceAs
 import kotlin.test.Test
 
@@ -14,5 +17,31 @@ class PersistentQueueTest {
         queue.count shouldBeExactly 0
         queue.front shouldBeSameInstanceAs PersistentList.Empty
         queue.back shouldBeSameInstanceAs PersistentVector.EmptyVector
+        queue shouldBeSameInstanceAs PersistentQueue<Int>()
+    }
+
+    @Test
+    fun `conj() should add an element to the front, when queue is empty`() {
+        val queue = PersistentQueue<Int>()
+
+        val newQueue = queue.conj(45)
+
+        newQueue.count shouldBeExactly 1
+        newQueue.front shouldBe l(45)
+        newQueue.back shouldBeSameInstanceAs PersistentVector.EmptyVector
+    }
+
+    @Test
+    fun `conj() should add an element to the back, when queue is non empty`() {
+        val queue = PersistentQueue<Int>().conj(45)
+
+        val newQueue = queue
+            .conj(90)
+            .conj(100)
+            .conj(200)
+
+        newQueue.count shouldBeExactly 4
+        newQueue.front shouldBe l(45)
+        newQueue.back shouldBe v(90, 100, 200)
     }
 }
