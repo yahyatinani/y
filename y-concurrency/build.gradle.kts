cc17fb49c9ba92a02aa078d56541355c58b3ffb3
Yahya <47183730+whyrising@users.noreply.github.com>
2021-11-17 14:13:13 +0100

    Update ktlint to 10.2.0 and snapshot version 0.0.9

--- y-concurrency/build.gradle.kts
+++ y-concurrency/build.gradle.kts
@@ -11,7 +11,7 @@ kotlin {
         jvm {
             compilations.all {
                 kotlinOptions {
-                    jvmTarget = "1.8"
+                    jvmTarget = Libs.jvmTargetVersion
                 }
             }
         }
