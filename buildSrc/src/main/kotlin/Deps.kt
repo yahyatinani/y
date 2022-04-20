6c7376115536b7f8d07512a88e4e83f24ab46388
Yahya <47183730+whyrising@users.noreply.github.com>
2022-04-20 15:07:30 +0000

    Update to kotlin 1.6.21

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -3,13 +3,13 @@ import org.gradle.api.JavaVersion
 object Deps {
     val jvmTarget = JavaVersion.VERSION_1_8.toString()
     const val kotlinApiVersion = "1.6"
-    const val kotlinVersion = "1.6.20"
+    const val kotlinVersion = "1.6.21"
 
     object Kotlinx {
         const val group = "org.jetbrains.kotlinx"
 
         object Atomicfu {
-            private const val version = "0.17.1"
+            private const val version = "0.17.2"
             const val atomicfu = "$group:atomicfu:$version"
             const val gradlePlugin = "$group:atomicfu-gradle-plugin:$version"
         }
@@ -20,7 +20,7 @@ object Deps {
         }
 
         object Coroutines {
-            private const val coroutinesV = "1.6.0"
+            private const val coroutinesV = "1.6.1"
             const val core = "$group:kotlinx-coroutines-core:$coroutinesV"
 
             const val test = "$group:kotlinx-coroutines-test:$coroutinesV"
@@ -28,7 +28,7 @@ object Deps {
     }
 
     object Kotest {
-        const val version = "5.2.2"
+        const val version = "5.2.3"
         const val runnerJvm = "io.kotest:kotest-runner-junit5-jvm:$version"
         const val assertions = "io.kotest:kotest-assertions-core:$version"
         const val propertyTest = "io.kotest:kotest-property:$version"
